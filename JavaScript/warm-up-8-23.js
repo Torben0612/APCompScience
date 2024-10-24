bd31ae3970df008b043f497885ae9088de549712
Torben0612 <74421064+Torben0612@users.noreply.github.com>
2024-10-24 08:25:41 -0400

    js

--- JavaScript/warm-up-8-23.js
+++ JavaScript/warm-up-8-23.js
@@ -1,3 +1,18 @@
-for (var i=0; i < 10; i++){
-    console.log(i)
+<<<<<<< HEAD
+//for (var i=0; i < 10; i++){
+//    console.log(i)
+//}
+
+function isPresent(students, name){
+    for (var i=0; i < students.length; i++){
+        if(students[i] == name){
+            return True
+        }
+    }
+    return false
 }
+
+var aBlock = ["Jamal", "Claire", "David", "Spiderman"] 
+console.log(isPresent(bBlock, "Alli")) //true
+console.log(isPresent(bBlock, "Lebron")) //false
+=======
