7e5591f2c7cb92eb660c8323f64082b33ff6bbd9
Torben0612 <74421064+Torben0612@users.noreply.github.com>
2024-11-06 09:24:30 -0500

    Update quiz.js

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -1,44 +1,56 @@
 var points = 0
 var questions = [
-  "Question1",
+  "which jupiter moon was detected to \nhave liquid water under the icy surface",
   "Question2",
   "question3",
   "question4",
   "Question5"]
 var answers = 
-    ["answer1",
+    ["Europa",
      "answer2",
      "answer3",
      "answer4",
      "answer5"]
-var result = "Test"
-currentQuestionNumber = 0
+var result = ""
+var currentQuestionNumber = 1
+var currentIndex = 0
 var question = ""
 var answerBox
+var logo
 
 function setup() {
   createCanvas(400, 400);
   answerBox = createInput();
 
   answerBox.position(100, 200)
+  logo = loadImage("background.png")
 }
 
 function draw() {
-  background(13, 135, 184)
-  textSize(25)
+  image(logo, 0,0, 400,400)
+  textSize(20)
   textAlign(CENTER)
-  text("Welcome to a Astronomy quiz", 200, 70)
-  text(result, 200, 300)
-  text("Question #" + currentQuestionNumber + 1, 200, 120)
+  rect(50,50,300,100)
+  text("Welcome to the Space Flight quiz", 200, 70)
   textSize(15)
-  text(questions[currentQuestionNumber], 200, 160)
+  text(result, 200, 300)
+  textSize(20)
+  text("Question #" + currentQuestionNumber, 200, 100)
+  textSize(15)
+  text(questions[currentIndex], 200, 120)
+  rect()
 }
 
-function keypressed(){
-  if (key == ENTER){
-    if ((answebox.Value) == answers[currentQuestionNumber]){
+function keyPressed(){
+  if (keyCode === ENTER){
+    if ((answerBox.Value) == answers[currentIndex]){
       points =+ 1
       result = "Correct you gained 1 point"
-    } else 
+      
+    } else {
+      result = "Incorrect the correct answer was " + answers[currentIndex + 1]
+      currentQuestionNumber += 1
+      currentIndex += 1
+    }
   }
 }
