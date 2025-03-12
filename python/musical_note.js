086d1d73336f1d8dfa3ccafd79bcd1d42afd02aa
Torben0612 <torben.krah@gmail.com>
2025-03-12 08:00:38 -0400

    did stuff

--- python/musical_note.js
+++ python/musical_note.js
@@ -1,38 +1,42 @@
-function decrypt(byte){
-    var pitch = "";
-    for(i = 0; i < 8; i++){
-        if(i<4){
-            pitch = byte[i];
-        } else if (i === 0){
+function decrypt(byte) {
+    var pitch = byte[1] + byte[2] + byte[3] + byte[4];
+    var duration = byte[4] + byte[5];
+    var octave = byte[6] + byte[7];
 
-    }
-    var finalNote = ""
-        if (pitch === "0000"){
-            return "A"
-        } else if(pitch === "0001"){
-            return "A#"
-        } else if(pitch === "0010"){
-            return "B"
-        } else if(pitch === "0011"){
-            return "C"
-        } else if(pitch === "0100"){
-            return "C#"
-        } else if(pitch === "0101"){
-            return "D"
-        } else if(pitch === "0110"){
-            return "D#"
-        } else if(pitch === "0111"){
-            return "E"
-        } else if(pitch === "1000"){
-            return "F"
-        } else if(pitch === "1001"){
-            return "F#"
-        } else if(pitch === "1010"){
-            return "G"
-        } else if(pitch === "1011"){
-            return "G#"
-        } else {
-            return "please upgrade to prenium version"
-        }
+    return getPitch(pitch) + " " + getDuration(duration) + " " + getOctave(octave);
 }
-decrypt("00000000")
+    function getPitch(code) {
+        if (code === "0000") { return "A"}
+        else if (code === "0001") { return "A#" }
+        else if (code === "0010") { return "B" }
+        else if (code === "0011") { return "C" }
+        else if (code === "0100") { return "C#" }
+        else if (code === "0101") { return "D" }
+        else if (code === "0110") { return "D#" }
+        else if (code === "0111") { return "E" }
+        else if (code === "1000") { return "F" }
+        else if (code === "1001") { return "F#" }
+        else if (code === "1010") { return "G" }
+        else if (code === "1011") { return "G#"}
+        else { return "please upgrade to premium version" }
+    }
+    function getDuration(code){
+        if (code === "00"){ return "1/1"}
+        else if (code === "01"){ return "1/2" }
+        else if (code === "10"){ return "1/4"}
+        else if (code === "11"){ return "1/8"}
+        else{ return "please upgrade to premium version" }
+    }
+
+    function getOctave(code){
+        if (code === "00") { return "1" }
+        else if (code === "01"){ return "2" }
+        else if (code === "10"){ return "3" }
+        else if (code === "11"){ return "4" }
+        else { return "please upgrade to premium version" }
+    }
+console.log(decrypt("00000000"));
+
+function encrypt(note){
+    var parts = note.split("")
+}
